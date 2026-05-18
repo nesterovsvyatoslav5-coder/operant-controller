46f3e68144e86bef47061e4b0bd081e9138099ed
nesterovsvyatoslav5-coder <nesterovsvyatoslav5@gmail.com>
2026-05-18 20:42:16 +0400

    serial_service.dart

--- lib/core/serial/serial_service.dart
+++ lib/core/serial/serial_service.dart
@@ -3,6 +3,11 @@ import 'package:flutter_libserialport/flutter_libserialport.dart';
 
 class SerialService {
   SerialPort? _port;
+  SerialPortReader? _reader;
+
+  List<String> getAvailablePorts() {
+    return SerialPort.availablePorts;
+  }
 
   bool connect(String portName) {
     _port = SerialPort(portName);
@@ -17,26 +22,25 @@ class SerialService {
       ..stopBits = 1
       ..parity = SerialPortParity.none;
 
+    _reader = SerialPortReader(_port!);
     return true;
   }
 
   void disconnect() {
+    _reader?.close();
     _port?.close();
+    _reader = null;
+    _port = null;
   }
 
   bool get isConnected => _port?.isOpen ?? false;
 
   void send(Uint8List data) {
+    if (!isConnected) return;
     _port?.write(data);
   }
 
-  Stream<Uint8List> listen() {
-    if (_port == null) {
-      throw Exception("Port not connected");
-    }
-
-    final reader = SerialPortReader(_port!);
-
-    return reader.stream;
+  Stream<Uint8List>? listen() {
+    return _reader?.stream;
   }
 }
